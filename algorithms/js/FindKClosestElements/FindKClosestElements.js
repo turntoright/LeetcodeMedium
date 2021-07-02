026a5ffe3b0ae0d7fc2b93534e0cf08a9d12fa5f
turntoright <hongwucao1975@gmail.com>
2021-07-02 23:21:27 +1200

    658. Find K Closest Elements

---
+++ algorithms/js/FindKClosestElements/FindKClosestElements.js
@@ -0,0 +1,10 @@
+/**
+ * @param {number[]} arr
+ * @param {number} k
+ * @param {number} x
+ * @return {number[]}
+ */
+ var findClosestElements = function(arr, k, x) {
+    arr.sort((a, b) => Math.abs(a-x) - Math.abs(b-x) === 0 ? a - b : Math.abs(a-x) - Math.abs(b-x));
+    return arr.slice(0, k).sort((a, b) => a - b);
+};
